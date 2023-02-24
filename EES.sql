22e8c5adca0c2749ddd08fb07b4cc9de33a4f6d1
Kirbyjam <123192314+Kirbyjam@users.noreply.github.com>
2023-02-24 09:41:54 -0700

    adds user table `User_Login`
    also Adds constrains, renames 4 columns,

--- EES.sql
+++ EES.sql
@@ -1,4 +1,4 @@
--- MySQL Workbench Forward Engineering
+/*-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `EES`.`Legacy_data` (
   `Expected_Voc_x_Isc_Watts` VARCHAR(45) NULL,
   `Infrared` VARCHAR(45) NULL,
   `Id` INT NULL,
-  INDEX `Id_idx` (`Id` ASC),/*had VISIBLE* keyword*/
+  INDEX `Id_idx` (`Id` ASC),/*had VISIBLE* keyword*//*
   CONSTRAINT `Legacy_Id`
     FOREIGN KEY (`Id`)
     REFERENCES `EES`.`Solar_Module_Tracking` (`Id`)
@@ -73,20 +73,20 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `EES`.`Defect_Modes` (
   `Corrosion_#_of_cells` INT NULL,
   `Cell_Cracks_#_of_cells` INT NULL,
-  `EVA_Browning` VARCHAR(45) NULL COMMENT 'None/Low/Medium/High/Extreme',
-  `Pattern of Browning` VARCHAR(45) NULL COMMENT 'Center/Edge/Other/None',
-  `Frame_Damage` CHAR(1) NULL COMMENT '0/1 0=no damage',
-  `Frame_Seal` CHAR(1) NULL,
-  `Jbox_Damage` CHAR(1) NULL,
-  `Jbox_Loose` CHAR(1) NULL,
-  `Nameplate_Faded/Missing` CHAR(1) NULL,
-  `Backside_Cracks` CHAR(1) NULL,
-  `Backside_Bubbles` CHAR(1) NULL,
-  ` Backside_Tears/Scratches` CHAR(1) NULL,
-  `Backside_Chalking` CHAR(1) NULL,
-  `Frontside_Burn_Mark` CHAR(1) NULL,
-  `Backside_Burn_Mark` CHAR(1) NULL,
-  `Scratch/Chip/Crack` CHAR(1) NULL,
+  `EVA_Browning` VARCHAR(45) NULL CHECK (EVA_Browning IN ('None','Low','Medium','High','Extreme')) COMMENT 'None/Low/Medium/High/Extreme',
+  `Pattern of Browning` VARCHAR(45) NULL CHECK(Pattern_of_Browning IN ('Center','Edge','Other','None')) COMMENT 'Center/Edge/Other/None', -- Renamed later to Pattern_of_Browning
+  `Frame_Damage` CHAR(1) NULL CHECK(Frame_Damage IN ('0','1')) COMMENT '0/1 0=no damage',
+  `Frame_Seal` CHAR(1) NULL CHECK(Frame_Seal IN ('0','1')),
+  `Jbox_Damage` CHAR(1) NULL CHECK(Jbox_damage IN ('0','1')),
+  `Jbox_Loose` CHAR(1) NULL CHECK(Jbox_Loose IN ('0','1')),
+  `Nameplate_Faded/Missing` CHAR(1) NULL CHECK(Nameplate_Faded/Missing IN ('0','1')), -- Renamed later to Nameplate_Faded/Missing
+  `Backside_Cracks` CHAR(1) NULL CHECK(JBackside_Cracks IN ('0','1')),
+  `Backside_Bubbles` CHAR(1) NULL CHECK(Backside_Bubbles IN ('0','1')),
+  `Backside_Tears/Scratches` CHAR(1) NULL CHECK(Backside_Tears/Scratches IN ('0','1')), -- Renamed later to Backside_Tears_Scratches
+  `Backside_Chalking` CHAR(1) NULL CHECK(Backside_Chalking IN ('0','1')),
+  `Frontside_Burn_Mark` CHAR(1) NULL CHECK(Frontside_Burn_Mark IN ('0','1')),
+  `Backside_Burn_Mark` CHAR(1) NULL CHECK(Backside_Burn_Mark IN ('0','1')),
+  `Scratch/Chip/Crack` CHAR(1) NULL CHECK(Scratch/Chip/Crack IN ('0','1')), -- Renamed later to Scratch_Chip_Crack
   `Delamination` INT NULL,
   `Milky_Discoloration` INT NULL,
   `Residual_Metal` INT NULL,
@@ -95,10 +95,10 @@ CREATE TABLE IF NOT EXISTS `EES`.`Defect_Modes` (
   `Future_Defect_1` INT NULL,
   `Future_Defect_2` INT NULL,
   `Future_Defect_3` INT NULL,
-  `Infrared` CHAR(1) NULL,
-  `Ultraviolet` CHAR(1) NULL,
+  `Infrared` CHAR(1) NULL  CHECK(Infrared IN ('0','1')),
+  `Ultraviolet` CHAR(1) NULL  CHECK(UltravioletFuture_Defect_1 IN ('0','1')),
   `Id` INT NULL,
-  INDEX `Id_idx` (`Id` ASC),/*had VISIBLE* keyword*/
+  INDEX `Id_idx` (`Id` ASC),/*had VISIBLE* keyword*//*
   CONSTRAINT `Defect_Id`
     FOREIGN KEY (`Id`)
     REFERENCES `EES`.`Solar_Module_Tracking` (`Id`)
@@ -116,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `EES`.`Final_Disposition` (
   `Dispose` VARCHAR(45) NULL,
   `Recycle` VARCHAR(45) NULL,
   `Id` INT NULL,
-  INDEX `Id_idx` (`Id` ASC),/*had VISIBLE* keyword*/
+  INDEX `Id_idx` (`Id` ASC),/*had VISIBLE* keyword*//*
   CONSTRAINT `Final_Id`
     FOREIGN KEY (`Id`)
     REFERENCES `EES`.`Solar_Module_Tracking` (`Id`)
@@ -125,6 +125,37 @@ CREATE TABLE IF NOT EXISTS `EES`.`Final_Disposition` (
 ENGINE = InnoDB;
 
 
+CREATE TABLE IF NOT EXISTS `EES`.`User_Login` (
+	Username VARCHAR(45) NOT NULL PRIMARY KEY,
+	Pass VARCHAR(45) NOT NULL
+);
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- Coulmes that were renamed
+ALTER TABLE defect_modes RENAME COLUMN `Pattern of Browning` TO Pattern_of_Browning;
+ALTER TABLE defect_modes RENAME COLUMN `Nameplate_Faded/Missing` TO Nameplate_Faded_Missing;
+ALTER TABLE defect_modes RENAME COLUMN ` Backside_Tears/Scratches` TO Backside_Tears_Scratches;
+ALTER TABLE defect_modes RENAME COLUMN `Scratch/Chip/Crack` TO Scratch_Chip_Crack;
+
+
+
+ALTER TABLE Defect_Modes ADD CONSTRAINT EVA_Browning CHECK (EVA_Browning IN ('None','Low','Medium','High','Extreme'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Pattern_of_Browning CHECK(Pattern_of_Browning IN ('Center','Edge','Other','None'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Frame_Damage CHECK(Frame_Damage IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Frame_Seal CHECK(Frame_Seal IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Jbox_Damage CHECK(Jbox_Damage IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Jbox_Loose CHECK(Jbox_Loose IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Nameplate_Faded_Missing CHECK(Nameplate_Faded_Missing IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Backside_Cracks CHECK(Backside_Cracks IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Backside_Bubbles CHECK(Backside_Bubbles IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Backside_Tears_Scratches CHECK(Backside_Tears_Scratches IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Backside_Chalking CHECK(Backside_Chalking IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Frontside_Burn_Mark CHECK(Frontside_Burn_Mark IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Backside_Burn_Mark CHECK(Backside_Burn_Mark IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Scratch_Chip_Crack CHECK(Scratch_Chip_Crack IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT Infrared CHECK(Infrared IN ('0','1'));
+ALTER TABLE Defect_Modes ADD CONSTRAINT  CHECK(Ultraviolet IN ('0','1'));*/
